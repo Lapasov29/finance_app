30eaa953e1f934b43af75a2f490496a8dccefdd3
anvar_lapasov <lapasovanvar2908@gmail.com>
2023-01-07 17:27:40 +0500

    connected to db

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
-console.log("Project has been started!!!");
-console.log("Project has been started!!!");
+import express from 'express'
+import {ENV} from "./common/config.js";
+import ConnectDb from "./common/db/connect.db.js"
+
+const app = new express()
+
+app.use(express.json())
+
+async function start(){
+    console.log('server is running')
+    ConnectDb()
+}
+
+app.listen(ENV.PORT, start())
