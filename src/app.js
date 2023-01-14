b007583fc6ac9984d920868eae2fb3dacc727392
anvar_lapasov <lapasovanvar2908@gmail.com>
2023-01-14 18:43:01 +0500

    dfas

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import {ENV} from "./common/config.js";
 import ConnectDb from "./common/db/connect.db.js"
 
-// routes
+// routesds
 import UserRoutes from "./finance/router/user/user.routes.js"
 import CashboxRoutes from "./finance/router/cashbox/cashbox.routes.js"
 import authToken from './finance/middleware/authToken.js';
